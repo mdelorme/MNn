33c008d0076fd390deff9760f7d597ab3ebb0757
Maxime Delorme <m.delorme@surrey.ac.uk>
2016-04-13 18:32:02 +0100

    Added a mock class to build the ref on RTD with the dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -15,10 +15,17 @@
 import sys
 import os
 
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.optimize', 'matplotlib.ticker', 'corner', 'emcee']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+
 
 # -- General configuration ------------------------------------------------
 
