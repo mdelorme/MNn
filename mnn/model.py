Bugfix : Negative mass discs should have a positive potential. Removed the absolute value from the potential computation.

mnn/model.py
@@ -13,8 +13,9 @@ class MNnError(Exception):
     def __str__(self):
         return self.msg
 
-G=4.302e-3
-#G = 0.0043008211
+
+#G=4.302e-3
+G = 0.0043008211
 """float: Gravitational constant to use when evaluating potential or forces on the models. 
 The value must be changed to match the units required by the user."""
 
@@ -212,7 +213,8 @@ class MNnModel(object):
             This method relies on user-specified value for the gravitational constant. 
             This value can be overriden by setting the value :data:`mnn.model.G`.
         """
-        M1 = np.sqrt(M**2)
+        #M1 = np.abs(M)
+        M1 = M
         h = np.sqrt(z**2 + b**2)
         den = r**2 + (a + h)**2
         return -G*M1 / np.sqrt(den)